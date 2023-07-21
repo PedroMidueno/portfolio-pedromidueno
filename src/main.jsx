55881807df3bef3ca7a0d3cb54c4be362168cc5a
Pedro Midueño <pedro.midueno@gmail.com>
2023-07-21 02:24:50 -0600

    Change root method for App

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import './index.css'
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 import { Provider } from 'react-redux';
 import { store } from './redux/store'
 
-
-ReactDOM.createRoot(document.getElementById("app")).render(
+const root = createRoot(document.getElementById("app"))
+root.render(
     <React.StrictMode>
         <Provider store={store}>
             <App />
