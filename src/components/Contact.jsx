918e54c8ea6d1d8d4d8e7771162deeaf86c4d22e
Pedro Midueño <pedro.midueno@gmail.com>
2023-09-09 14:12:10 -0600

    Add styles of Contact component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,47 +1,71 @@
 import styles from '../styles/Contact.module.css'
-import { linkedinLink, whatsappLink, LIGHT } from '../config/consts'
+import { linkedinLink, whatsappLink, LIGHT, ES } from '../config/consts'
 import { linkedin, whatsapp } from '../assets/images'
 import { useUISettings } from '../hooks/useUISettings'
 
 const Contact = () => {
-  const { theme } = useUISettings()
+  const { theme, lang } = useUISettings()
 
   return (
     <section className={styles.container}>
       {/*  eslint-disable-next-line */}
-      <h3 className={`${styles.title} ${theme === LIGHT && styles.light}`}>Ponte en contacto conmigo 😃​</h3>
-      <form action='https://formsubmit.co/65129fde3b1949ece8b83c82be690bdf' method='POST'>
+      <h3 className={`${styles.title} ${theme === LIGHT && styles.light}`}>
+        {lang === ES ? 'Ponte en contacto conmigo 😃' : 'Get in touch with me 😃'}
+
+      </h3>
+      <form action='https://formsubmit.co/65129fde3b1949ece8b83c82be690bdf' method='POST' className={`${theme === LIGHT && styles.light}`}>
         <div className={styles.name_div}>
           <input type='text' name='name' placeholder=' ' id='name' className={`${styles.name_input} ${theme === LIGHT && styles.light}`} required />
-          <label htmlFor='name' className={`${styles.name_label} ${theme === LIGHT && styles.light}`}>Nombre:</label>
+          <label htmlFor='name' className={`${styles.name_label} ${theme === LIGHT && styles.light}`}>
+            {lang === ES ? 'Nombre:' : 'Name:'}
+          </label>
         </div>
         <div className={styles.email_div}>
           <input type='email' name='email' placeholder=' ' id='email' className={`${styles.email_input} ${theme === LIGHT && styles.light}`} required />
-          <label htmlFor='email' className={`${styles.email_label} ${theme === LIGHT && styles.light}`}>Email:</label>
+          <label htmlFor='email' className={`${styles.email_label} ${theme === LIGHT && styles.light}`}>
+            {lang === ES ? 'Correo electrónico:' : 'Email:'}
+          </label>
         </div>
         <div className={styles.subject_div}>
           <input type='text' name='subject' placeholder=' ' id='subject' className={`${styles.subject_input} ${theme === LIGHT && styles.light}`} required />
-          <label htmlFor='subject' className={`${styles.subject_label} ${theme === LIGHT && styles.light}`}>Asunto:</label>
+          <label htmlFor='subject' className={`${styles.subject_label} ${theme === LIGHT && styles.light}`}>
+            {lang === ES ? 'Asunto:' : 'Subject:'}
+          </label>
         </div>
         <div className={styles.message_div}>
           <textarea name='message' id='message' placeholder=' ' cols='30' rows='5' className={`${styles.message_input} ${theme === LIGHT && styles.light}`} required />
-          <label htmlFor='message' className={`${styles.message_label} ${theme === LIGHT && styles.light}`}>Mensaje:</label>
+          <label htmlFor='message' className={`${styles.message_label} ${theme === LIGHT && styles.light}`}>
+            {lang === ES ? 'Tu mensaje:' : 'Your message:'}
+          </label>
         </div>
 
-        <button type='submit' className={styles.submit_button}>Enviar</button>
+        <button type='submit' className={`${styles.submit_button} ${theme === LIGHT && styles.light}`}>
+          {lang === ES ? 'Enviar' : 'Submit'}
+        </button>
 
         <input type='hidden' name='_next' value='http://localhost:3000/thanks' />
         <input type='hidden' name='_captcha' value='false' />
       </form>
 
-      <span>O bien puedes contactarme a través de los siguientes medios:</span>
-      <div className={styles.buttons}>
-        <a href={linkedinLink} target='_blank' rel='noreferrer'>
-          <img src={linkedin} alt='' />
+      <div className={`${styles.separador} ${theme === LIGHT && styles.light}`}>
+        <hr />
+        <span>
+          {lang === ES ? 'ó' : 'or'}
+        </span>
+      </div>
+
+      <span className={styles.social}>
+        {lang === ES
+          ? 'Puedes contactarme a través de los siguientes medios:'
+          : 'You can contact me through the following means:'}
+      </span>
+      <div className={styles.links}>
+        <a href={linkedinLink} target='_blank' rel='noreferrer' className={`${styles.link} ${theme === LIGHT && styles.light}`}>
+          <img src={linkedin} alt='' title='LinkedIn' />
         </a>
 
-        <a href={whatsappLink} target='_blank' rel='noreferrer'>
-          <img src={whatsapp} alt='' />
+        <a href={whatsappLink} target='_blank' rel='noreferrer' className={`${styles.link} ${theme === LIGHT && styles.light}`}>
+          <img src={whatsapp} alt='' title='WhatsApp' />
         </a>
       </div>
     </section>
